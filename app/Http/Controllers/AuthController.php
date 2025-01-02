6e0f67250fb1ccc4d810fe1eb0d06b8f4540579b
Piter33 <pitertrintaetres@gmail.com>
2025-01-02 15:33:00 -0300

    feat: created AuthController and auth API routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        //
+    }
+
+    public function getUser()
+    {
+        //
+    }
+
+    public function logout()
+    {
+        //
+    }
+
+    public function refresh()
+    {
+        //
+    }
+
+    public function register(Request $request)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1/auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+
+    Route::middleware([JwtMiddleware::class])->group(function () {
+        Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+        Route::get('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+        Route::get('/user', [AuthController::class, 'getUser'])->name('auth.user');
+    });
+});
