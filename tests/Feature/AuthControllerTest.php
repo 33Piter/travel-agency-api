493f7bde4cbd0e84b2e9deea8279cf18d3e630fb
Piter33 <pitertrintaetres@gmail.com>
2025-01-02 16:37:00 -0300

    test: added tests for login auth route

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -2,12 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tests for register method
+    |--------------------------------------------------------------------------
+    |
+    */
 
     public function test_user_can_register()
     {
@@ -38,5 +47,44 @@ class AuthControllerTest extends TestCase
 
         $response->assertStatus(400)
             ->assertJsonStructure(['name', 'email', 'password']);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tests for login method
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    public function test_user_can_login_with_valid_credentials()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->postJson(route('auth.login'), [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['token']);
+    }
+
+    public function test_login_fails_with_invalid_credentials()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->postJson(route('auth.login'), [
+            'email' => 'test@example.com',
+            'password' => 'wrong-password',
+        ]);
+
+        $response->assertStatus(401)
+            ->assertJson(['message' => 'Invalid credentials']);
     }
 }
