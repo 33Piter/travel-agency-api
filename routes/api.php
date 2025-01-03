f734dd729beca3eade3ec773ac155178635cfbbe
Piter33 <pitertrintaetres@gmail.com>
2025-01-03 10:49:00 -0300

    feat: added travel order api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TravelOrderController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +13,12 @@ Route::prefix('v1/auth')->group(function () {
         Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
         Route::get('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
         Route::get('/user', [AuthController::class, 'getUser'])->name('auth.user');
+    });
+});
+
+Route::prefix('v1')->group(function () {
+    Route::middleware([JwtMiddleware::class])->group(function () {
+        Route::apiResource('travel-order', TravelOrderController::class)->except(['destroy']);
+        Route::get('travel-order/notify/{travel_order}', [TravelOrderController::class, 'notify'])->name('travel-order.notify');
     });
 });
