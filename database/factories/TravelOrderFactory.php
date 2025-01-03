4fac3a6f6a6630d77a673d27c8be69991a565df1
Piter33 <pitertrintaetres@gmail.com>
2025-01-03 09:39:00 -0300

    feat: created TravelOrderFactory and updated DatabaseSeeder

--- database/factories/TravelOrderFactory.php
+++ database/factories/TravelOrderFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\TravelOrderStatusEnum;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,17 @@ class TravelOrderFactory extends Factory
      */
     public function definition(): array
     {
+        $departureDate = $this->faker->dateTimeBetween('now', '+1 year');
+        $returnDate = $this->faker->dateTimeBetween($departureDate, '+1 year');
+
         return [
-            //
+            'user_id' => User::inRandomOrder()->value('id'),
+            'applicant_name' => $this->faker->name,
+            'applicant_email' => $this->faker->email,
+            'destination' => $this->faker->country,
+            'departure_date' => $departureDate->format('Y-m-d'),
+            'return_date' => $returnDate->format('Y-m-d'),
+            'status' => fake()->randomElement(TravelOrderStatusEnum::cases()),
         ];
     }
 }
